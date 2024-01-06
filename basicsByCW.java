401ab35d4f5f6280b34e727bf2f768cfe5659024
Subhash Pathak <132152652+CoderSubhashPathak@users.noreply.github.com>
2024-01-06 19:49:54 +0530

    Add files via upload

---
+++ basicsByCW.java
@@ -0,0 +1,209 @@
+package BasicsOfJava;
+import java.util.Scanner;
+
+public class basicsByCW {
+    public static void main(String [] args){
+
+        System.out.println("Welcome to Subhash Pathak's Coding World");
+        System.out.println("Welcome Upcoming Amazon SDE1 Subhash Pathak");
+        System.out.println("Hello Subh");
+        System.out.print("Subhash ");
+        System.out.println("Pathak");
+
+        //Printing Numbers
+
+        System.out.println("23");
+        System.out.println(4);
+        System.out.println(4+3);
+        System.out.println("4+3");
+        System.out.println("Hello World");
+
+
+        /*
+        // Variables
+
+        int x;
+        x = 3;
+        System.out.println(x);
+
+        x = 5;
+        System.out.println(x);
+
+        x = 12;
+       System.out.println(x+1); //13
+        x = x + 5;
+        System.out.println(x); //17
+
+        x = x - 10;
+        System.out.println(x); // 7
+
+        int a = 5;
+        System.out.println(a); //5
+
+        a = 7;
+        System.out.println(a); //7
+
+        a = a + 6; // 13
+        System.out.println(a);
+
+        a = a - 20; // -7
+        System.out.println(a);
+
+
+        // arithmetic operation on int data type
+
+        int num1 = 12;
+        int num2 = 15;
+        System.out.println(num1 + num2);
+        System.out.println(num1 - num2);
+        System.out.println(num1 * num2);
+        System.out.println(num1 / num2);
+
+        num1 = 23;
+        num1 = 12;
+        int num3 = 8;
+
+        System.out.println("The Value of num1 is equals to : " + num1);
+        System.out.println("Sum " + num1 + num2); // Sum 2312
+        System.out.println(" " + num1 + num2 + num3);
+        System.out.println(num1 + num2 + " " + num3); // first num1 + num2 than back space and 8
+             */
+
+
+        // comments
+        // single line comment
+
+        /*
+
+        //  multiple comments
+
+          */
+        /**/
+
+
+
+        // Increment decrement Operators
+
+       // int x = 5;
+
+      /*  System.out.println(x);
+
+         x++;     // x = x + 1
+        System.out.println(x); //6
+
+        x--;
+        System.out.println(x); //5
+
+        ++x;
+        System.out.println(x); // 6
+
+        --x;
+        System.out.println(x);  // 5
+        */
+       /*
+        System.out.println(x);
+        System.out.println(x++); // 5 but x = 6
+        System.out.println(x); //6
+
+        System.out.println(++x); // 7
+
+        int num1 = 12;
+        int num2 = 14;
+        int num3 = 21;
+        int sum = num1 + num2 + num3;
+        System.out.println("sum of these numbers " + sum);
+        */
+
+        // double data type
+
+        double x = 23.34234d;
+        int y = 32;
+        double z = x + y;
+        System.out.println(z);
+
+
+        // Calculating percentage of 5 subjects
+
+        Scanner subh = new Scanner(System.in);
+
+        /*
+        System.out.println("Enter your marks in physics");
+        double phy = subh.nextDouble();
+
+        System.out.println("Enter your marks in chemistry");
+        double chem = subh.nextDouble();
+
+        System.out.println("Enter your marks in mathematics");
+        double math = subh.nextDouble();
+
+        System.out.println("Enter your marks in hindi");
+        double hindi = subh.nextDouble();
+
+        System.out.println("Enter your marks in english");
+        double eng = subh.nextDouble();
+
+        double percentage = (phy + chem + math + hindi + eng)/5;
+        System.out.println("This is your percentage " + percentage);
+ */
+
+        //Area of a circle
+/*
+        System.out.println("enter radius of a circle");
+        double radius = subh.nextDouble();
+        double pi = 3.14;
+        double ac = pi * radius * radius;
+        System.out.println("Area of a circle is " + ac);
+  */
+        // Calculate simple intrest
+
+        /*
+        System.out.println("enter value of p ");
+        double p = subh.nextDouble();
+        System.out.println("enter value of q" );
+        double r = subh.nextDouble();
+        System.out.println("enter value of t" );
+        double t = subh.nextDouble();
+        double si = (p*r*t)/100;
+        System.out.println("si is " + si);
+*/
+
+        System.out.println("What is your name");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your age");
+        int yourAge = sc.nextInt();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+}
